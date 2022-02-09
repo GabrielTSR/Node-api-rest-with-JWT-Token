761defb8b0c2a6ffa5f180e8ffdacc97f41cbedf
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-02-08 21:06:51 -0300

    real email sending finished

--- src/app/modules/mailer.js
+++ src/app/modules/mailer.js
@@ -7,9 +7,13 @@ const { host, port, user, pass } = require('../../config/mail.json');
 const transport = nodemailer.createTransport({
     host,
     port,
+    secure: false,
     auth: {
         user,
         pass,
+    },
+    tls: {
+        rejectUnauthorized: false,
     },
 });
 
